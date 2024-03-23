0ee37440d89194969238a04baa0e127a46d87d61
Nisha Singhal <nishasinghal979@gmail.com>
2024-03-23 09:47:54 +0530

    check health of server

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", checkHealth)
 	server := http.Server{
-		Addr:    ":8000",
+		Addr:    ":8080",
 		Handler: corsMux,
 	}
 	server.ListenAndServe()
+}
+
+func checkHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+	w.WriteHeader(200)
 }
 
 func middlewareCors(next http.Handler) http.Handler {
